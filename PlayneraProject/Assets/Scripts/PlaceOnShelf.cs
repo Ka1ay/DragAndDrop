756d719be144d486f991eee8653bccde199254f3
Ka1ay <halimov03@mail.ru>
2025-01-15 11:16:14 +0300

    UPD1

---
+++ PlayneraProject/Assets/Scripts/PlaceOnShelf.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlaceOnShelf : MonoBehaviour
+{
+    private Vector3 offset;
+    private bool isDragging = false;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMouseDown()
+    {
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        offset = transform.position - GetMouseWorldPosition();
+        isDragging = true;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition() + offset;
+    }
+
+    private void OnMouseUp()
+    {
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        isDragging = false;
+
+        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero);
+        if (hit.collider != null && hit.collider.CompareTag("Shelf"))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, hit.collider.transform.position.z);
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        mouseScreenPosition.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+    }
+}
+
+
---
+++ PlayneraProject/Assets/Scripts/ScrollScene.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SceneScroll : MonoBehaviour
+{
+    public float scrollSpeed = 10f;
+    public float minX = -10f;
+    public float maxX = 10f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Mouse X");
+
+        Vector3 newPosition = transform.position + new Vector3(moveX, 0, 0) * scrollSpeed * Time.deltaTime;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+        transform.position = newPosition;
+    }
+}
+
